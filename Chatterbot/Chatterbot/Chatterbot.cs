31d5bf6eac46400b419f3f45dd4d979f729bd0ae
Gibson Benham <grshaneb@gmail.com>
2015-02-15 16:20:31 -0700

    first commit

---
+++ Chatterbot/Chatterbot/Chatterbot.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChatterbotProject
+{
+    class Chatterbot
+    {                
+        enum State
+        {
+            Default,
+            NoMatch,
+            FirstMatch,
+            SecondMatch,
+            Success,
+            Fail
+        };
+
+        public bool Chat(string input)
+        {
+            State currentState = State.Default;
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch(currentState)
+                {
+                    case (State.Default):
+                        if (input[i] == 'h')
+                            currentState = State.FirstMatch;
+                        else if (input[i] == ' ')
+                            currentState = State.Default;
+                        else
+                            currentState = State.NoMatch;
+                        break;
+                    case (State.FirstMatch):
+                        if (input[i] == 'i')
+                            currentState = State.SecondMatch;
+                        else if (input[i] == ' ' || input[i] == '.')
+                            currentState = State.Default;
+                        else
+                            currentState = State.NoMatch;
+                        break;
+                    case (State.SecondMatch):
+                        if (input[i] == ' ' || input[i] == '.')
+                            currentState = State.SecondMatch;
+                        else
+                            currentState = State.NoMatch;
+                        break;
+                    case (State.NoMatch):
+                        if (input[i] == ' ' || input[i] == '.')
+                            currentState = State.Default;
+                        else
+                            currentState = State.NoMatch;
+                        break;
+                }
+            }
+            return (currentState == State.SecondMatch);
+        }
+    }
+}
---
+++ Chatterbot/Chatterbot/ChatterbotTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ChatterbotProject
+{
+    [TestFixture]
+    public class ChatterbotTest
+    {
+        [Test]
+        [TestCase("hi", true)]
+        [TestCase("hi.", true)]
+        [TestCase("hi ", true)]
+        [TestCase(" hi", true)]
+        [TestCase("hhi", false)]
+        [TestCase("h hi", true)]
+        [TestCase("I said hi", true)]
+        [TestCase("this", false)]
+        public void TestChatterbot(string input, bool expectedResult)
+        {
+            Chatterbot chat = new Chatterbot();
+            Assert.AreEqual(expectedResult, chat.Chat(input));
+        }
+
+    }
+}
